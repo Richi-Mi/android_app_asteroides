1c84b3aee8864d813b87128343703027a2897e84
José Ricardo Mendoza Castañeda <joseanakin@outlook.es>
2022-12-26 17:03:09 -0600

    FATAL, no se que pasa

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.asteroides;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_constraint);
+        setContentView( R.layout.activity_main_constraint );
 
+        // Accediendo a los valores de los recursos de la app en la carpeta Res.
+        Resources res = getResources();
+
+        Drawable drawable = res.getDrawable(android.R.drawable.alert_dark_frame);
+        Drawable icon     = res.getDrawable( R.drawable.ic_launcher_foreground );
+        String titulo     = res.getString( R.string.Asteroides );
+        int color         = res.getColor( R.color.black );
+        float fontSize    = res.getDimension( R.dimen.tamaño_fuente );
+        boolean limit     = res.getBoolean( R.bool.limite );
+        String[] dias     = res.getStringArray( R.array.dias_semana );
     }
 
 }
